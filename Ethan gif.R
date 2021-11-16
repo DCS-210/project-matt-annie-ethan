bdb2f283d304496bf0dbd6bec380f90bb874ef58
mlclayton6 <mclayton@bates.edu>
2021-11-16 02:21:21 +0000

    Finalize animation (Ethan5)

--- Ethan gif.R
+++ Ethan gif.R
@@ -1,9 +1,6 @@
-ethan <- ggplot(data = alldata_dist, mapping = aes(y = sy_distance,
-                                              x = disc_year,
-                                              color = discoverymethod
-                                              )) +
-  
-  
+ethan5 <- ggplot(data = alldata_dist, mapping = aes(y = sy_distance,
+                                                   x = disc_year,
+                                                   color = discoverymethod)) +
   geom_jitter(alpha = 1) + 
   labs(
     y = "Distance of Discovery (Parsecs)",
@@ -11,16 +8,21 @@ ethan <- ggplot(data = alldata_dist, mapping = aes(y = sy_distance,
     title = "Planet mass and distance of discovery",
     subtitle="By discovery method" ) +
   theme(legend.position = "bottom") +
-  theme(legend.title = element_blank()) 
+  theme(legend.title = element_blank()) +
+  theme(legend.key.size = unit(0.3, 'cm')) +
+  theme(legend.spacing = unit(0.2, 'cm')) + 
+  theme(legend.text=element_text(size=9))
 
-ethan_anim <- ethan +
-              transition_time(as.integer(disc_year)) +
-              shadow_mark() +
-              labs(title = "Discovery Year: {frame_time}") +
-              enter_fade() +
-              exit_reset() +
+ethan5
+
+ethan_anim <- ethan2 +
+  transition_time(as.integer(disc_year)) +
+  shadow_mark() +
+  labs(title = "Discovery Year: {frame_time}") +
+  enter_fade() +
+  exit_reset() +
                       
 
 
-anim_save("Ethan.gif", ethan_anim, width = 830, height = 500)
+anim_save("Ethan5.gif", ethan_anim, width = 650)
 
