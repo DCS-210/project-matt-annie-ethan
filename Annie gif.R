5a399209615452cac87e80ccb35968c8da6065ba
AnnieL2 <ali2@bates.edu>
2021-12-04 17:27:08 +0000

    Update repo organization

--- Annie gif.R
+++
@@ -1,8 +0,0 @@
-
-map_data <- 
-  base_world +
-  geom_point(data=facility_df2, 
-             aes(x=facility_longitude, y=facility_latitude,size = count,fill=facility_type), colour = "black",
-             pch=21, alpha=I(0.5)) +
-  scale_size_continuous(range = c(3, 30)) +
-    gganimate(map_data)
